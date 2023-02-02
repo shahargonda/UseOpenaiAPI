8232192ed96e6b41003bff59790484237d8e09dd
Shahar Gonda <shahargonda@gmail.com>
2023-02-02 16:01:08 +0200

    regular completion, saving to dataset

--- UseOpenaiAPI.py
+++ UseOpenaiAPI.py
@@ -5,8 +5,8 @@ import yaml
 import openai
 from openai.embeddings_utils import get_embedding, cosine_similarity
 import numpy as np
-from flask import Flask, request
-from twilio.twiml.messaging_response import MessagingResponse
+#from flask import Flask, request
+#from twilio.twiml.messaging_response import MessagingResponse
 import pandas as pd
 
 # Open the yaml config file and load the variables
@@ -30,6 +30,7 @@ REGULAR_COMPLETIONS_API_PARAMS = {
     "model": COMPLETIONS_MODEL,
 }
 
+'''
 app = Flask(__name__)
  
 @app.route("/wa")
@@ -40,8 +41,9 @@ def wa_hello():
 def wa_sms_reply():
     """Respond to incoming calls with a simple text message."""
     # Fetch the message
-    
-    msg = request.form.get('Body').lower() # Reading the message from the whatsapp
+
+    msg= input('input something!: ')
+    #msg = request.form.get('Body').lower() # Reading the message from the whatsapp
     # Get the message time
     now = datetime.now()
     dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
@@ -130,6 +132,84 @@ def return_most_similiar(question, df, top_n=3):
     # Get the index of the top 3 most similar message
     most_similiar = df.sort_values('similarity', ascending=False).head(top_n)
     return most_similiar
+'''
+
+def return_most_similiar(question, df, top_n=3):
+    # Get the embedding for the question
+    question_embedding = get_embedding(question, engine='text-embedding-ada-002')
+    # Get the embedding for the messages in the database
+    df["ada_search"] = df["ada_search"].apply(eval).apply(np.array)
+    # Get the similarity between the question and the messages in the database
+    df['similarity'] = df.ada_search.apply(lambda x: cosine_similarity(x, question_embedding))
+    # Get the index of the top 3 most similar message
+    most_similiar = df.sort_values('similarity', ascending=False).head(top_n)
+    return most_similiar
+
+def generate_context(question, df, top_n=3):
+    most_similiar = return_most_similiar(question, df, top_n)
+    # Get the top 3 most similar messages
+    top_messages = most_similiar["message"].values
+    # Concatenate the top 3 messages into a single string
+    context = '\n '.join(top_messages)
+    return context
+
+def construct_prompt(question, df, top_n=3):
+    # Get the context
+    context = generate_context(question, df, top_n)
+    header =  header = """Answer the question in details, based only on the provided context and nothing else, and if the answer is not contained within the text below, say "I don't know.", do not invent or deduce!\n\nContext:\n"""
+    return header + "".join(context) + "Q: " + question + "\n A:"
+
+def process_message(msg):
+    # Get the message time
+    now = datetime.now()
+    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+
+    # Load the database
+    df = pd.read_csv(PATH + "database.csv")
+
+    resp = ""
+    reply_body = ""
+
+    # Help menu
+    if msg.startswith("/h"):
+        reply_body= "Commands:\n\n/q [question] - Ask a question\n/s [message] - Save a message\n/f [message] - Find related messages\n/h - Show this help menu"
+
+        # Save the message
+    elif msg.startswith("/s "):
+        data_to_save = msg.split("/s ")[1]
+        # Save the massage to the database
+        text_embedding = get_embedding(data_to_save, engine='text-embedding-ada-002')
+        df = df.append({"time": dt_string, "message": data_to_save, "ada_search": text_embedding}, ignore_index=True)
+        df.to_csv(PATH + "database.csv", index=False)
+        reply_body= "Message saved successfully!"
+    # Get a regular completion
+    else:
+        # Just get a regular completion from the model
+        response = openai.Completion.create(prompt=msg, **REGULAR_COMPLETIONS_API_PARAMS)
+        #print(response)
+        reply_body= response["choices"][0]["text"]
+
+    return reply_body
+
+def input_loop():
+    continue_flag=True
+    while (continue_flag==True):
+        msg = input("input something! ('terminate' to stop):").lower()
+        print ("you've inputted:", msg)
+
+        # termination
+        if msg == 'terminate':
+            print('Thanks for being around!')
+            continue_flag=False
+        else:
+            # else: create reply
+            reply_body= process_message(msg)
+            print (reply_body)
+
+
+
+
+
 
 if __name__ == "__main__":	
 
@@ -141,4 +221,5 @@ if __name__ == "__main__":
         # Save the dataframe to a csv file
         df.to_csv(PATH+"database.csv",index=False)
 
-    app.run(debug=True)
+    input_loop()
+    #app.run(debug=True)
