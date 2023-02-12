dd99348bdc2fde8ee88d32cbd45eac5580bb6999
Shahar Gonda <shahargonda@gmail.com>
2023-02-12 11:24:06 +0200

    wrapper function over all API calls to prevent SLA problems

--- UseOpenaiAPI.py
+++ UseOpenaiAPI.py
@@ -107,36 +107,26 @@ def wa_sms_reply():
         reply.body(response["choices"][0]["text"])
         
     return str(resp)
-
-def construct_prompt(question, df, top_n=3):
-    # Get the context
-    context = generate_context(question, df, top_n)
-    header =  header = """Answer the question in details, based only on the provided context and nothing else, and if the answer is not contained within the text below, say "I don't know.", do not invent or deduce!\n\nContext:\n"""
-    return header + "".join(context) + "Q: " + question + "\n A:"
-
-def generate_context(question, df, top_n=3):
-    most_similiar = return_most_similiar(question, df, top_n)
-    # Get the top 3 most similar messages
-    top_messages = most_similiar["message"].values
-    # Concatenate the top 3 messages into a single string
-    context = '\n '.join(top_messages)
-    return context
-
-def return_most_similiar(question, df, top_n=3):
-    # Get the embedding for the question
-    question_embedding = get_embedding(question, engine='text-embedding-ada-002')
-    # Get the embedding for the messages in the database
-    df["ada_search"] = df["ada_search"].apply(eval).apply(np.array)
-    # Get the similarity between the question and the messages in the database
-    df['similarity'] = df.ada_search.apply(lambda x: cosine_similarity(x, question_embedding))
-    # Get the index of the top 3 most similar message
-    most_similiar = df.sort_values('similarity', ascending=False).head(top_n)
-    return most_similiar
 '''
 
+def retry(func):
+    def wrapper(*args, **kwargs):
+        delay_time = kwargs.pop("delay_on_exception", 10)
+        max_retries = kwargs.pop("max_retries", 5)
+        exception = kwargs.pop("exception", Exception)
+        for _ in range(max_retries):
+            try:
+                return func(*args, **kwargs)
+            except exception as e:
+                print(f"Exception: {e} raised. Retrying...")
+                time.sleep(delay_time)
+        return func(*args, **kwargs)
+    return wrapper
+
+
 def return_most_similiar(question, df, top_n=3):
     # Get the embedding for the question
-    question_embedding = get_embedding(question, engine='text-embedding-ada-002')
+    question_embedding = retry(get_embedding)(question, engine='text-embedding-ada-002')
     # Get the embedding for the messages in the database
     df["ada_search"] = df["ada_search"].apply(eval).apply(np.array)
     # Get the similarity between the question and the messages in the database
@@ -157,7 +147,7 @@ def construct_prompt(question, df, top_n=3):
     # Get the context
     context = generate_context(question, df, top_n)
     header =  header = """Answer the question in details, based only on the provided context and nothing else, and if the answer is not contained within the text below, say "I don't know.", do not invent or deduce!\n\nContext:\n"""
-    return header + "".join(context) + "Q: " + question + "\n A:"
+    return header + "".join(context) + "\nQ: " + question + "\n A:"
 
 def process_message(msg, verbose=0):
     # Get the message time
@@ -167,7 +157,6 @@ def process_message(msg, verbose=0):
     # Load the database
     df = pd.read_csv(PATH + "database.csv")
 
-    resp = ""
     reply_body = ""
 
     # Help menu
@@ -178,7 +167,7 @@ def process_message(msg, verbose=0):
     elif msg.startswith("/s "):
         data_to_save = msg.split("/s ")[1]
         # Save the massage to the database
-        text_embedding = get_embedding(data_to_save, engine='text-embedding-ada-002')
+        text_embedding = retry(get_embedding)(data_to_save, engine='text-embedding-ada-002')
         if verbose:
             print ('original df size:', df.shape)
         df= pd.concat ([df, pd.DataFrame({"time": dt_string, "message": data_to_save, "ada_search": [text_embedding]})], ignore_index=True, axis=0)
@@ -186,6 +175,15 @@ def process_message(msg, verbose=0):
             print('new df size:', df.shape)
         df.to_csv(PATH + "database.csv", index=False)
         reply_body= "Message saved successfully!"
+
+    # Find related messages
+    elif msg.startswith("/f "):
+        query = msg.split("/f ")[1]
+        most_similar = return_most_similiar(query, df, top_n=3)
+        msg_reply = ''
+        for i in range(len(most_similar)):
+            msg_reply += most_similar.iloc[i]['time'] + ': ' + most_similar.iloc[i]['message'] + '\n'
+        reply_body(msg_reply)
 
     # Question answering
     elif msg.startswith("/q "):
@@ -195,12 +193,17 @@ def process_message(msg, verbose=0):
         prompt = construct_prompt(question, df, top_n=3)
         print("I'm gonna ask that:\n", prompt)
         # Get the answer
-        response = openai.Completion.create(prompt=prompt, **QUESTION_COMPLETIONS_API_PARAMS)
+        response = retry(openai.Completion.create)(prompt=prompt, **QUESTION_COMPLETIONS_API_PARAMS)
         reply_body= response["choices"][0]["text"]
+
+    # Placeholder for other commands
+    elif msg.startswith("/"):
+        reply.body("Sorry, I don't understand the command")
+
     # Get a regular completion
     else:
         # Just get a regular completion from the model
-        response = openai.Completion.create(prompt=msg, **REGULAR_COMPLETIONS_API_PARAMS)
+        response = retry(openai.Completion.create)(prompt=msg, **REGULAR_COMPLETIONS_API_PARAMS)
         if verbose:
             print(response)
         reply_body= response["choices"][0]["text"]
@@ -222,11 +225,6 @@ def input_loop():
             reply_body= process_message(msg)
             print (reply_body)
 
-
-
-
-
-
 if __name__ == "__main__":	
 
     openai.api_key = OPENAI_KEY
@@ -238,4 +236,6 @@ if __name__ == "__main__":
         df.to_csv(PATH+"database.csv",index=False)
 
     input_loop()
-    #app.run(debug=True)
+
+
+
